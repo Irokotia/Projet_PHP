11d868b9706c0d3060fde7430d3551800cce234f
BOISSIN Rowin <boissinr@hostname.iut2.upmf-grenoble.fr>
2018-10-18 10:40:56 +0200

    create.sql

--- PROJET_PHP/Modèle/create.sql
+++ PROJET_PHP/Modèle/create.sql
@@ -1,9 +1,7 @@
 create Table Processeur (
   id INTEGER,
-  author STRING,
-  title STRING,
-  cover STRING,
-  mp3 STRING,
-  category STRING
-
+  marque STRING,
+  modele STRING,
+  reference STRING,
+  Ghz STRING
 );
