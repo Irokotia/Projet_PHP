fde7b945513b81202c32ff378b4dadbff5c255cd
BOISSIN Rowin <boissinr@hostname.iut2.upmf-grenoble.fr>
2018-10-22 09:24:38 +0200

    Dappppefe

--- PROJET_PHP/Modèle/CarteMèreDAO.class.php
+++ PROJET_PHP/Modèle/CarteMèreDAO.class.php
@@ -3,8 +3,8 @@ class CarteMèreDAO
 {
 	private $db;
 
-	function __construct($path) {
-		$database = 'sqlite:'.$path.'/cartemère.db';
+	function __construct() {
+		$database = 'sqlite:data/cartemère.db';
 		try {
 			$this->db = new PDO($database);
 		} catch (PDOException $e) {
@@ -15,7 +15,7 @@ class CarteMèreDAO
 	function get(int $id):CarteMère {
 		$req="SELECT * FROM cartemère WHERE id='$id'";
 		$sth=($this->db)->query($req);
-		$result = $sth->fetchAll(PDO::FETCH_CLASS,'Music');
+		$result = $sth->fetchAll(PDO::FETCH_CLASS,'CarteMère');
 		return $result[0];
 	}
 }
