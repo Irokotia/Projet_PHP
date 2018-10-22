0a1ecf296c17ff9feb1ae9721ff6ddbf47d71d93
BOISSIN Rowin <boissinr@hostname.iut2.upmf-grenoble.fr>
2018-10-22 09:18:22 +0200

    last modif

---
+++ PROJET_PHP/Modèle/CarteMèreDAO.test.php
@@ -0,0 +1,13 @@
+<?php
+require_once('CarteMère.class.php');
+require_once('CarteMèreDAO.class.php');
+
+// Récupération des données de configuration
+$config = parse_ini_file('../config/config.ini');
+
+//Création de l'instance DAO
+$CarteMère = new CarteMèreDAO($config['database_path']);
+
+$cm = $CarteMère->get(1);
+var_dump($cm);
+ ?>
