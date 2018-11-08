b08cdd111a0eb27ffa9be200c917fbea9a6de08e
MARTIN Alexandre <martin27@hostname.iut2.upmf-grenoble.fr>
2018-11-08 20:00:45 +0100

    veve

--- PROJET_PHP/Vues/CarteMère.vues.php
+++ PROJET_PHP/Vues/CarteMère.vues.php
@@ -69,6 +69,7 @@
 
             <a href="CarteMère2.ctrl.php?id=<?php echo $id ?>">
             <img src="<?= $url ?>" />
+            <?= $list2 ?>
               </div>
             </a>
            <?php } ?>
