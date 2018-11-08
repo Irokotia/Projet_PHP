47b2b7c1d4d1e0ab26e44f575732adc5d520ef99
MARTIN Alexandre <martin27@hostname.iut2.upmf-grenoble.fr>
2018-11-08 17:28:00 +0100

    ef

--- PROJET_PHP/Modèle/CarteMèreDAO.class.php
+++ PROJET_PHP/Modèle/CarteMèreDAO.class.php
@@ -4,7 +4,7 @@ class CarteMèreDAO
 	private $db;
 
 	function __construct($path) {
-		$database = 'sqlite:'.$path.'/cartemère.db';
+		$database = 'sqlite:'.$path.'/cartemere.db';
 		try {
 			$this->db = new PDO($database);
 		} catch (PDOException $e) {
@@ -13,7 +13,7 @@ class CarteMèreDAO
 	}
 
 	function get(int $id):CarteMère {
-		$req="SELECT * FROM cartemère WHERE id='$id'";
+		$req="SELECT * FROM cartemere WHERE id='$id'";
 		$sth=($this->db)->query($req);
 		$result = $sth->fetchAll(PDO::FETCH_CLASS,'CarteMère');
 		return $result[0];
