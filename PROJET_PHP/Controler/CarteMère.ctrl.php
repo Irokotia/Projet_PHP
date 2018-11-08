91dfe725d054a4249410afabad7eadb84059098e
BOISSIN Rowin <boissinr@hostname.iut2.upmf-grenoble.fr>
2018-11-08 20:31:48 +0100

    sqdqsdq

--- PROJET_PHP/Controler/CarteMère.ctrl.php
+++ PROJET_PHP/Controler/CarteMère.ctrl.php
@@ -22,9 +22,8 @@ $pageApres = $page+1;
 $config = parse_ini_file('../config/config.ini');
 
 $CarteMère = new CarteMèreDAO($config['database_path']);
-
 for($i = 1+($nbCarteMère*($page - 1)); $i < ($nbCarteMère+1)+($nbCarteMère*($page - 1)); $i++){
 	// Récupération de l'objet CarteMère
 	$c = $CarteMère->get($i);
-	// Ajout à la liste des images à afficher
-	$list[$i] = $c;
+// Ajout à la liste des images à afficher
+	$list[i] = $c;
