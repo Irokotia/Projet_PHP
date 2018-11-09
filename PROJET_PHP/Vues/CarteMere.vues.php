d33f0d4d50c58f1cb7415ee31abcc7561f8e522c
MARTIN Alexandre <martin27@hostname.iut2.upmf-grenoble.fr>
2018-11-09 10:07:13 +0100

    nty

--- PROJET_PHP/Vues/CarteMere.vues.php
+++ PROJET_PHP/Vues/CarteMere.vues.php
@@ -63,6 +63,14 @@
               <h2>Liste des produits</h2>
           </div>
 
+          <a href="CarteMere.ctrl.php?firstId=<?= $pageAvant ?>&nbCarteMère=<?= $nbCarteMère ?>">
+          <img src="../Vues/Images/ImagesSwitch/Actions-arrow-left-icon.png" alt="Flèche gauche"></a>
+          <?= $page ?>
+          <a href ="CarteMere.ctrl.php?firstId=1">
+          <img src="../Vues/Images/ImagesSwitch/Actions-arrow-top-icon.png" alt="Menu principal"></a>
+          <a href="CarteMere.ctrl.php?firstId=<?= $pageApres ?>&nbCarteMère=<?= $nbCarteMère ?>">
+          <img src="../Vues/Images/ImagesSwitch/Actions-arrow-right-icon.png" alt="Flèche droite"></a></p>
+
           <?php foreach($list as $id => $url){   ?>
             <div class="box">
             <a href="CarteMere2.ctrl.php?id=<?php echo $id ?>">
@@ -76,14 +84,6 @@
               </div>
             </a>
            <?php } ?>
-
-           <a href="CarteMere.ctrl.php?firstId=<?= $pageAvant ?>&nbCarteMère=<?= $nbCarteMère ?>">
-           <img src="../Vues/Images/ImagesSwitch/Actions-arrow-left-icon.png" alt="Flèche gauche"></a>
-           <?= $page ?>
-           <a href ="CarteMere.ctrl.php?firstId=1">
-           <img src="../Vues/Images/ImagesSwitch/Actions-arrow-top-icon.png" alt="Menu principal"></a>
-           <a href="CarteMere.ctrl.php?firstId=<?= $pageApres ?>&nbCarteMère=<?= $nbCarteMère ?>">
-           <img src="../Vues/Images/ImagesSwitch/Actions-arrow-right-icon.png" alt="Flèche droite"></a></p>
 
         </div>
         <div id="footer">
